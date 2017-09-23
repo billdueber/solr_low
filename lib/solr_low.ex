18dd3eba312a785ce7c8b7198d8247fa145a462c
Bill Dueber <bill@dueber.com>
2017-09-22 22:21:45 -0400

    ResponseHeader as ecto working

--- lib/solr_low.ex
+++ lib/solr_low.ex
@@ -1,58 +1,11 @@
 require Tesla
 
-defmodule SolrReply do
-    @derive [Poison.Encoder]
-    defstruct [:responseHeader, :response]
-
-  defmodule ResponseHeader do
-    @derive [Poison.Encoder]
-    defstruct [:status, :QTime, :params]
-  end
-
-  defmodule Response do
-    @derive [Poison.Encoder]
-    defstruct [:docs, :numFound, :start]
-  end
-
-  @type reply_decode_type ::    %SolrReply{responseHeader: %SolrReply.ResponseHeader{},
-                                           response: %SolrReply.Response{}}
-
-
-  def qtime(sr) do
-    Map.get sr.responseHeader, :QTime
-  end
-
-  def numFound(sr) do
-    sr.response.numFound
-  end
-
-  def start(sr) do
-    sr.response.start
-  end
-
-  def docs(sr) do
-    sr.response.docs
-  end
-
-  def params(sr) do
-    sr.responseHeader.params
-  end
-
-  def status(sr) do
-    sr.responseHeader.status
-  end
-
-  def count(sr) do
-    sr |> docs |> Enum.count
-  end
-end
-
 defmodule SolrLow do
   use Tesla
 
   plug Tesla.Middleware.Query, [wt: "json"]
   plug Tesla.Middleware.Headers, %{"User-Agent" => "ElixirSolrLow"}
-  plug Tesla.Middleware.JSON, engine_opts: %{as: SolrReply.reply_decode_type}
+#  plug Tesla.Middleware.JSON
 
 
   plug Tesla.Middleware.Logger
@@ -67,7 +20,7 @@ defmodule SolrLow do
   def keyword(client, kw, value) do
     y = SolrLow.get(client, "select", query: [q: "#{kw}:#{value}"])
     case y.status do
-      200 -> y.body
+      200 -> SolrLow.SolrReply.from_json  y.body
       _ -> {:error, y.status, y}
     end
 
--- lib/solr_reply.ex
+++ lib/solr_reply.ex
@@ -1,19 +1,20 @@
-defmodule SolrLow.SolrResponse do
+defmodule SolrLow.SolrReply do
 
   use Ecto.Schema
   import Ecto.Changeset
 
   @primary_key false
   embedded_schema do
-    embeds_one :responseHeader, SolrLow.SolrResponse.ResponseHeader
+    embeds_one :responseHeader, SolrLow.SolrReply.ResponseHeader
     field :response, :map
+
   end
 
   def from_json(data) when is_binary(data) do
-    Poison.decode!(data) |> from_json
+    Poison.decode!(data) |> from_map
   end
 
-  def from_json(data) when is_map(data) do
+  def from_map(data) when is_map(data) do
     y = %__MODULE__{}
     |> cast(data, [:response])
     |> cast_embed(:responseHeader)
@@ -23,7 +24,7 @@ defmodule SolrLow.SolrResponse do
   end
 end
 
-defmodule SolrLow.SolrResponse.ResponseHeader do
+defmodule SolrLow.SolrReply.ResponseHeader do
   use Ecto.Schema
   import Ecto.Changeset
 
@@ -45,13 +46,24 @@ defmodule SolrLow.SolrResponse.ResponseHeader do
     end
 
   end
+
+  defmodule SolrLow.SolrReply.Response do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @primary_key false
+    embedded_schema do
+      field :numFound, :integer
+      field :count,  :float, virtual: true
+      field :docs, {:array, :map}
+      field :start, :integer
+
+      def changeset(struct, data) do
+        struct
+        |> cast(data, [:numFound, :docs, :start])
+      end
+    end
+
+  end
 end
 
-defmodule SolrLow.SolrResponse.ResponseHeader do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  @primary_key false
-   
-
-end
